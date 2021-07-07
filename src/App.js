0e0a519e8745cc42607f1547a138cf884bcbd525
Patrickrgt <patrickrgt@gmail.com>
2021-07-06 23:35:14 -0400

    mobile changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 import Nav from "./components/nav.js";
+import NavMobile from "./components/navmobile.js";
 
 function App() {
+  if (window.innerWidth >= 426) {
+    return <Nav></Nav>;
+  } else if (window.innerWidth <= 425) {
+    return <NavMobile></NavMobile>;
+  }
   return (
     <div className="App">
       <Nav></Nav>
